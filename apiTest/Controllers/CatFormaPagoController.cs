2fca99a7d84d377352a300fa9e19655848cdb2cd
jalvaromedina <64150889+jalvaromedina@users.noreply.github.com>
2025-06-16 00:48:43 -0700

    correccion de endpoint
    se hace correccion de endpoint de obtener formas de pago

--- apiTest/Controllers/CatFormaPagoController.cs
+++ apiTest/Controllers/CatFormaPagoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Voalaft.API.Servicios.Implementacion;
 using Voalaft.API.Servicios.Interfaces;
 using Voalaft.API.Utils;
@@ -6,7 +7,8 @@ using Voalaft.Data.Entidades;
 
 namespace Voalaft.API.Controllers
 {
-    //[Authorize]
+    
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class CatFormaPagoController : Controller
